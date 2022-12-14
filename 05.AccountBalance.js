a6542865836dc32a8527d56c0fae489fdb91dda4
Ivan Spasov <ivan4o8999@gmail.com>
2022-12-14 21:53:46 +0200

    Add files via upload

---
+++ 05.AccountBalance.js
@@ -0,0 +1,26 @@
+function solve(input) {
+    let deposit = input[0];
+    let index = 1;
+    let balance = 0;
+
+    while (deposit !== "NoMoreMoney") {
+        let amount = Number(deposit);
+
+        if (amount < 0) {
+            console.log("Invalid operation!");
+            break;
+        }
+
+        balance = balance + amount;
+        console.log(`Increase: ${amount.toFixed(2)}`);
+        deposit = input[index];
+        index++;
+    }
+    console.log(`Total: ${balance.toFixed(2)}`);
+}
+
+solve([
+"5.51", 
+"69.42",
+"100",
+"NoMoreMoney"]);
---
+++ 06.MaxNumber.js
@@ -0,0 +1,22 @@
+function solve(input) {
+    let n = input[0];
+    let index = 1;
+    let max = Number.MIN_SAFE_INTEGER;
+
+    while (n !== "Stop") {
+        let num = Number(n);
+        if (num > max) {
+            max = num;
+        }
+        n = input[index];
+        index++
+    }
+    console.log(max);
+}
+
+solve([
+"100",
+"99",
+"80",
+"70",
+"Stop"]);
